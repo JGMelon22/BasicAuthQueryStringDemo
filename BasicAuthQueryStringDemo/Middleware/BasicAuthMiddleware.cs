0a1aa297b55621510fbc09c0fb388f50be8ce6f4
JGMelon22 <jgvasconcellos22@gmail.com>
2025-01-18 13:47:48 -0300

    refactor: use early return principle

--- BasicAuthQueryStringDemo/Middleware/BasicAuthMiddleware.cs
+++ BasicAuthQueryStringDemo/Middleware/BasicAuthMiddleware.cs
@@ -25,51 +25,7 @@ public class BasicAuthMiddleware
     {
         string base64Credentials = context.Request.Query["credentials"].ToString();
 
-        if (!string.IsNullOrEmpty(base64Credentials))
-        {
-            try
-            {
-                byte[] decodedBytes = Convert.FromBase64String(base64Credentials);
-                string decodedString = Encoding.UTF8.GetString(decodedBytes);
-
-                string[] credentials = decodedString.Split(':');
-                if (credentials.Length == 2)
-                {
-                    string decodedUsername = credentials[0];
-                    string decodedPassword = credentials[1];
-
-                    string configuredUsername = _appSettings.Value.Username;
-                    string configuredPassword = _appSettings.Value.Password;
-
-                    if (decodedUsername == configuredUsername && decodedPassword == configuredPassword)
-                    {
-                        _logger.LogInformation("Authentication successful for user: {Username}", decodedUsername);
-                    }
-                    else
-                    {
-                        _logger.LogWarning("Invalid credentials provided.");
-                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                        await context.Response.WriteAsync("Unauthorized.");
-                        return;
-                    }
-                }
-                else
-                {
-                    _logger.LogWarning("Invalid Base64 credentials format.");
-                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
-                    await context.Response.WriteAsync("Invalid Base64 credentials format.");
-                    return;
-                }
-            }
-            catch (FormatException ex)
-            {
-                _logger.LogError("Error decoding Base64 string: {Error}", ex.Message);
-                context.Response.StatusCode = StatusCodes.Status400BadRequest;
-                await context.Response.WriteAsync("Invalid Base64 credentials.");
-                return;
-            }
-        }
-        else
+        if (string.IsNullOrEmpty(base64Credentials))
         {
             _logger.LogWarning("No credentials found in the query string.");
             context.Response.StatusCode = StatusCodes.Status401Unauthorized;
@@ -77,6 +33,44 @@ public class BasicAuthMiddleware
             return;
         }
 
-        await _next(context);
+        try
+        {
+            byte[] decodedBytes = Convert.FromBase64String(base64Credentials);
+            string decodedString = Encoding.UTF8.GetString(decodedBytes);
+            string[] credentials = decodedString.Split(':');
+
+            if (credentials.Length != 2)
+            {
+
+                _logger.LogWarning("Invalid Base64 credentials format.");
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                await context.Response.WriteAsync("Invalid Base64 credentials format.");
+                return;
+            }
+
+            string decodedUsername = credentials[0];
+            string decodedPassword = credentials[1];
+            string configuredUsername = _appSettings.Value.Username;
+            string configuredPassword = _appSettings.Value.Password;
+
+            if (decodedUsername != configuredUsername || decodedPassword != configuredPassword)
+            {
+                _logger.LogWarning("Invalid credentials provided.");
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Unauthorized.");
+                return;
+            }
+
+            _logger.LogInformation("Authentication successful for user: {Username}", decodedUsername);
+            await _next(context);
+
+        }
+        catch (FormatException ex)
+        {
+            _logger.LogError("Error decoding Base64 string: {Error}", ex.Message);
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            await context.Response.WriteAsync("Invalid Base64 credentials.");
+            return;
+        }
     }
 }
